55e41e0b9cfbab39b80e67a552b628481e1939a7
Roman <roman.l.senn@gmail.com>
2020-01-29 13:43:55 +0100

    ...

--- bh-badge-animate.c
+++ bh-badge-animate.c
@@ -3,85 +3,91 @@
 uint8_t ballX = 4;
 uint8_t ballY = 9;
 
-//Hint: look in HaD_Badge.h for function and constant definitions
+// Hint: look in HaD_Badge.h for function and constant definitions
 
-void eraseBall() {
-    //uses global variables to erase current ball
-    displayPixel(ballX, ballY, OFF);
-    displayLatch();
+void
+eraseBall() {
+  // uses global variables to erase current ball
+  displayPixel(ballX, ballY, OFF);
+  displayLatch();
 }
 
-void moveLeft() {
-    if (ballX > 0) {
-        //only move if we're not already at the edge
-        eraseBall();
-        --ballX;
-        displayPixel(ballX, ballY, ON);
-        displayLatch();
-    }
-}
-
-void moveRight() {
-    if (ballX < TOTPIXELX-1) {
-        //only move if we're not already at the edge
-        eraseBall();
-        ++ballX;
-        displayPixel(ballX, ballY, ON);
-        displayLatch();
-    }
-}
-
-void moveUp() {
-    if (ballY > 0) {
-        //only move if we're not already at the edge
-        eraseBall();
-        --ballY;
-        displayPixel(ballX, ballY, ON);
-        displayLatch();
-    }
-}
-
-void moveDown() {
-    if (ballY < TOTPIXELY-1) {
-        //only move if we're not already at the edge
-        eraseBall();
-        ++ballY;
-        displayPixel(ballX, ballY, ON);
-        displayLatch();
-    }
-}
-
-void animateBadge(void) {
-
+void
+moveLeft() {
+  if(ballX > 0) {
+    // only move if we're not already at the edge
+    eraseBall();
+    --ballX;
     displayPixel(ballX, ballY, ON);
     displayLatch();
-    uint32_t lastTime = getTime();
+  }
+}
 
-    while(1) {
-        
-        //This shows how to use non-blocking getTime() function
-        if (getTime() > lastTime+1000) {
-            lastTime = getTime();
-            Buffer[14] ^= 0xFF;
-        }
-        
-        //This shows how to get user input
-        switch (getControl()) {
-            case (ESCAPE):
-                displayClose();
-                return;
-            case (LEFT):
-                moveLeft();
-                break;
-            case (RIGHT):
-                moveRight();
-                break;
-            case (UP):
-                moveUp();
-                break;
-            case (DOWN):
-                moveDown();
-                break;
-        }
+void
+moveRight() {
+  if(ballX < TOTPIXELX - 1) {
+    // only move if we're not already at the edge
+    eraseBall();
+    ++ballX;
+    displayPixel(ballX, ballY, ON);
+    displayLatch();
+  }
+}
+
+void
+moveUp() {
+  if(ballY > 0) {
+    // only move if we're not already at the edge
+    eraseBall();
+    --ballY;
+    displayPixel(ballX, ballY, ON);
+    displayLatch();
+  }
+}
+
+void
+moveDown() {
+  if(ballY < TOTPIXELY - 1) {
+    // only move if we're not already at the edge
+    eraseBall();
+    ++ballY;
+    displayPixel(ballX, ballY, ON);
+    displayLatch();
+  }
+}
+
+void
+animateBadge(void) {
+
+  displayPixel(ballX, ballY, ON);
+  displayLatch();
+  uint32_t lastTime = getTime();
+
+  while(1) {
+
+    // This shows how to use non-blocking getTime() function
+    if(getTime() > lastTime + 1000) {
+      lastTime = getTime();
+      Buffer[14] ^= 0xFF;
     }
+
+    // This shows how to get user input
+    switch(getControl()) {
+      case(ESCAPE):
+        displayClose();
+        return;
+      case(LEFT):
+        moveLeft();
+        break;
+      case(RIGHT):
+        moveRight();
+        break;
+      case(UP):
+        moveUp();
+        break;
+      case(DOWN):
+        moveDown();
+        break;
+    }
+  }
 }
--- bh-badge-animate.h
+++ bh-badge-animate.h
@@ -1,11 +1,11 @@
- #include "HaD_Badge.h"
+#include "HaD_Badge.h"
 
-//Add your function protopes:
+// Add your function protopes:
 void eraseBall(void);
 void moveLeft(void);
 void moveRight(void);
 void moveUp(void);
 void moveDown(void);
 
-//This function prototype needs to be here:
+// This function prototype needs to be here:
 void animateBadge(void);
